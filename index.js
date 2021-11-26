b8957f960e0afb9a9af59a19b62513c8c5712b09
Rinor Rama <rinor.rama95@gmail.com>
2021-11-26 10:57:49 +0100

    can delete todos and some code refactoring

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const state = {
-	showTodos: true,
+	showCompltedTodos: true,
 	todos: [
 		{
 			title: "Buy Milk",
@@ -22,17 +22,27 @@ const state = {
 	],
 };
 
+//derived States
+function getincompleteTodos() {
+	return state.todos.filter(function (todo) {
+		return !todo.completed;
+	});
+}
+
+function getCompletedTodos() {
+	return state.todos.filter(function (todo) {
+		return todo.completed;
+	});
+}
+
+//rendering functions
 function render() {
 	renderCompleteTodos();
 	renderIncomplteteTodos();
 }
-
 function renderCompleteTodos() {
-	const todoListUl = document.querySelector(".todo-list");
 	todoListUl.innerHTML = "";
-	const incompleteTodos = state.todos.filter(function (todo) {
-		return !todo.completed;
-	});
+	const incompleteTodos = getincompleteTodos();
 	for (const todo of incompleteTodos) {
 		const liEl = createTodoLiEl(todo);
 		todoListUl.append(liEl);
@@ -40,15 +50,11 @@ function renderCompleteTodos() {
 }
 
 function renderIncomplteteTodos() {
-	const completedSection = document.querySelector(".completed-section");
-	if (state.showTodos) {
+	if (state.showCompltedTodos) {
 		completedSection.style.display = "block";
-		const completedListUl = document.querySelector(".completed-list");
 		completedListUl.innerHTML = "";
-		const completeTodos = state.todos.filter(function (todo) {
-			return todo.completed;
-		});
-		for (const todo of completeTodos) {
+		const completedTodos = getCompletedTodos();
+		for (const todo of completedTodos) {
 			const liEl = createTodoLiEl(todo);
 			completedListUl.append(liEl);
 		}
@@ -56,20 +62,24 @@ function renderIncomplteteTodos() {
 		completedSection.style.display = "none";
 	}
 }
-
+// create todo list element function
 function createTodoLiEl(todo) {
 	const liEl = document.createElement("li");
-	liEl.classList.add("todo");
+	liEl.className = "todo";
+	if (todo.completed) {
+		liEl.classList.add("completed");
+	}
 
 	const checkboxSection = document.createElement("div");
 	checkboxSection.className = "checkbox-section";
 	const checkboxEl = document.createElement("input");
 	checkboxEl.className = "completed-checkbox";
 	checkboxEl.setAttribute("type", "checkbox");
+	checkboxEl.checked = todo.completed;
 	checkboxSection.append(checkboxEl);
 
 	checkboxEl.addEventListener("click", function () {
-		todo.completed = !todo.completed;
+		toggleTodo(todo);
 		render();
 	});
 
@@ -89,38 +99,53 @@ function createTodoLiEl(todo) {
 	deleteButton.className = "delete";
 	deleteButton.textContent = "delete";
 	buttonSection.append(editButton, deleteButton);
+	deleteButton.addEventListener("click", function () {
+		deleteTodo(todo);
+		render();
+	});
 
 	liEl.append(checkboxSection, textSection, buttonSection);
 
-	if (todo.completed) {
-		liEl.classList.add("completed");
-		checkboxEl.checked = true;
-	}
 	return liEl;
 }
 
-function addNewTodo(title) {
-	const newTodo = {
-		title: title,
-		completed: false,
-		tag: "Test",
-		user: "Rinor",
-	};
-	state.todos.unshift(newTodo);
+//action functions
+function addTodo(todo) {
+	state.todos.unshift(todo);
 }
+function toggleTodo(todo) {
+	todo.completed = !todo.completed;
+}
+function deleteTodo(todo) {
+	const indexToDeltete = state.todos.indexOf(todo);
+	state.todos.splice(indexToDeltete, 1);
+}
+function toggleShowCompletedTodos() {
+	state.showCompltedTodos = !state.showCompltedTodos;
+}
+
+const todoListUl = document.querySelector(".todo-list");
+const completedSection = document.querySelector(".completed-section");
+const completedListUl = document.querySelector(".completed-list");
 
 render();
 
 const showCompletedCheckbox = document.querySelector(".show-completed-checkbox");
 showCompletedCheckbox.addEventListener("click", function () {
-	state.showTodos = !state.showTodos;
+	toggleShowCompletedTodos();
 	render();
 });
 
 const addForm = document.querySelector(".add-item");
 addForm.addEventListener("submit", function (event) {
 	event.preventDefault();
-	addNewTodo(addForm.text.value);
+	const newTodo = {
+		title: addForm.text.value,
+		completed: false,
+		tag: "Test",
+		user: "Rinor",
+	};
+	addTodo(newTodo);
 	addForm.reset();
 	render();
 });
